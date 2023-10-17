5534e58c9bd37090df26ca590c360afaf1be620f
meshack34 <meshackkimutai34@gmail.com>
2023-10-17 17:30:43 +0300

    all

---
+++ chat/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.shortcuts import render, redirect
+
+ 
+def chatPage(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        return redirect("login-user")
+    context = {}
+    return render(request, "chat/chatPage.html", context)
